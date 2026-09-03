a4750f430bae54e24b453ff9ca26194a1138529b
Arpitojha1 <arpitozha@gmail.com>
2026-09-03 12:11:04 +0530

    feat(landing): compose HeroCircles into Home page hero section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,18 @@
 import { Hero } from '../components/Hero';
 import { RecapSection } from '../components/RecapSection';
+import { HeroCircles } from '../components/landing/HeroCircles';
 
 export function Home() {
   return (
     <main className="w-full">
-      <Hero />
+      {/*
+        Relative wrapper so HeroCircles can be absolutely positioned
+        against the hero bounds without restructuring Hero's internals.
+      */}
+      <div className="relative">
+        <Hero />
+        <HeroCircles />
+      </div>
       <RecapSection />
     </main>
   );
